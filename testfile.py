e896991001998261765de2283b33fd4bafc45dbf
iCxt <101859095+iCxts@users.noreply.github.com>
2025-11-04 15:02:30 +0700

    Delete testfile.py

--- testfile.py
+++
@@ -1,9 +0,0 @@
-# print(bin((1 << 24) - 1 << 8))
-# #print(((i << mask) - 1) << (32 - mask))
-
-#     print(print_from_u32(netmask(mask32)))
-#     print(print_from_u32(wildcard(mask32)))
-#     print(print_from_u32(f"{network(ip32, mask32)}/{mask}"))
-#     print(print_from_u32(f"{broadcast(ip32, mask32)}"))
-#     print(print_from_u32(first_host(ip32, mask32, mask)))
-#     print(print_from_u32(last_host(ip32, mask32, mask)))
